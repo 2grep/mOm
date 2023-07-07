940c081d7c598765649a28156073c82c8c5f610d
blob1234567890 <ben.k.olson@gmail.com>
2023-07-07 00:28:21 -0700

    moved step functions out of main and made independent

--- alpha.py
+++ alpha.py
@@ -46,255 +46,276 @@ def test(datasets) -> tuple[np.ndarray[stats.rv_continuous], list[tuple[int]], f
     return (fits, fails, end - start)
 
 
+def get_args(data_root: str):
+    args = {}
+    args["res"] = {}
+    args["data_paths"] = (
+        f"./data/{data_root}/assisted_5class.npy",
+        f"./data/{data_root}/unassisted_5class.npy",
+    )
+    args["results_directory"] = f"./results/{data_root}/"
+    args["graphing"] = {
+        "colors": ["red", "green"],
+        "dims": (4, 6),     # Counts of rows and cols; Want observers x cases
+        "ratio": (2, 1),    # Horizontal vs. vertical aspect ratio
+        "scale": 1.4,
+        "histogram_bins": 30
+    }
+    args["alpha_error"] = .05
+    return args
+
+def get_data(data_paths: list[str]):
+    print("Fetching data...")
+    datasets = [np.load(data) for data in data_paths]
+    datasets = np.transpose(np.asarray(datasets), (0, 3, 2, 1)) # (assisted/unassisted, observers, cases, surfaces)
+    print("Got datasets")
+    return datasets
+
+def run_fits(datasets):
+    print("Running fits...")
+
+    #                                                 Use one (1) of these (a b)
+    # res["test"] = test(datasets)                  # -+
+    #                                                  |- a -
+    # betas = res["test"][0]                        # -+
+    betas = np.apply_along_axis(fit, -1, datasets)   # --- b -
+
+    print("Fits finished.")
+    return betas
+
+def get_graphs(
+        datasets,
+        betas,
+        args: dict
+) -> str:
+    obs = np.flip(np.linspace(
+        datasets.shape[1] - 1, 0, 
+        num=args["dims"][0],
+        endpoint=False,
+        dtype=int
+    )) # obs should count up
+    cases = np.linspace(
+        datasets.shape[2] - 1, 0,
+        num=args["dims"][1],
+        endpoint=False,
+        dtype=int
+    ) # cases should count down (to match the top-left [0, 0] of axs)
+    fig, axs = plt.subplots(
+        ncols=args["dims"][0], 
+        nrows=args["dims"][1], 
+        figsize=(args["ratio"][0] * args["scale"] * args["dims"][0], args["ratio"][1] * args["scale"] * args["dims"][1]),
+        layout="constrained"
+    )
+    x = np.linspace(0, 1, num=500)
+    constant_indices = True
+    is_cdf = False
+    with np.nditer(
+        axs,
+        flags=[
+            "multi_index",
+            "refs_ok"
+        ],
+        op_flags=[
+            "readwrite"
+        ],
+        op_dtypes=matplotlib.axes._axes.Axes
+    ) as it:
+        for ax in it:
+            ax = ax.item()
+
+            ind = (
+                obs[it.multi_index[1]], 
+                cases[it.multi_index[0]]
+            )
+            # Adjust indices to skip None in betas
+            while any([betas[g, *ind] == None for g in range(datasets.shape[0])]):
+                # increment cases if needed
+                ind = (ind[0], ind[1] + 1)
+                constant_indices = False
+
+            ax.set_xlabel(ind[0] + 1)
+            ax.set_ylabel(ind[1] + 1)
+            for group in range(datasets.shape[0]):
+                # Finally, graph emperical (hist) and theoretical (plot) pdfs for each SARAPE surface
+                group_ind = (group, *ind)
+                hist = ax.hist(
+                    datasets[group_ind[0], group_ind[1], group_ind[2]], 
+                    bins=args["histogram_bins"], 
+                    range=(0, 1), 
+                    align="mid",
+                    density=True, # Draw normalized so area == 1
+                    cumulative=is_cdf, # Draw as CDF
+                    color=args["colors"][group],
+                    alpha=.5
+                )[0]
+
+                if is_cdf:
+                    y = betas[group_ind[0], group_ind[1], group_ind[2]].cdf(x)
+                else:
+                    y = betas[group_ind[0], group_ind[1], group_ind[2]].pdf(x)
+
+                ax.plot(
+                    x, y,
+                    color=args["colors"][group]
+                )
+
+    # Clear out excess axes display elements
+    with np.nditer(
+        axs,
+        flags=[
+            "multi_index",
+            "refs_ok"
+        ],
+        op_flags=[
+            "readwrite"
+        ],
+        op_dtypes=matplotlib.axes._axes.Axes
+    ) as it:
+        bottom_row = args["dims"][1] - 1
+        left_col = 0
+        for ax in it:
+            # For some god forsaken reason, ax is a zero-dimensional array which python just DOESN'T want to treat right
+            # We use `ndarray.item()` to get the Python scalar value
+            ax = ax.item()
+            is_bottom_row = it.multi_index[0] == bottom_row
+            is_left_col = it.multi_index[1] == left_col
+            ax.tick_params(
+                axis="both",
+                which="both",
+                top=False,
+                bottom=is_bottom_row,
+                left=False,
+                right=False,
+                labeltop=False,
+                labelbottom=is_bottom_row,
+                labelleft=False,
+                labelright=False,
+            )
+            
+            if constant_indices:
+                if not is_left_col:
+                    ax.set_ylabel("")
+                if not is_bottom_row:
+                    ax.set_xlabel("")
+
+    graph_form = "CDF" if is_cdf else "PDF"
+    fig.suptitle(f"Various SARAPE Emperical vs. Theoretical {graph_form}s")
+    fig.supxlabel("Observer Count")
+    fig.supylabel("Case Count")
+    return graph_form
+
+def run_kstest(betas, datasets):
+    def unwrapped_kstest(x: np.ndarray[stats.rv_continuous, float]):
+        '''
+        Apply single-sided kstest from NDArray. Necessary for using apply_along_axis
+        since we can't otherwise use two parallel arrays
+
+        x : NDArray
+            Of the form `[stats.rv_continuous, float64, ..., float64]`
+        
+        Returns
+        -------
+        KstestResult (see `scipy.stats`)
+        '''
+        beta = x[0]
+        if beta == None:
+            return None
+        data = x[1:].astype('f')
+        return stats.kstest(data, beta.cdf)
+
+    combo = np.concatenate((betas[..., np.newaxis], datasets), axis=-1)
+    return np.apply_along_axis(unwrapped_kstest, -1, combo)
+
+def run_emperical_vs_theoretical_comparison(
+        betas,
+        alpha_error: float
+):
+    '''
+    Returns
+    -------
+    Tuple of, in order, theoretical beta and cutoff along with difference between emperical and theoretical
+    '''
+    print("Running cutoffs...", flush=True)
+    theoretical = np.transpose(
+        np.apply_along_axis(
+            lambda betas, alpha_error=.05: compare(*betas, alpha_error=alpha_error),
+            0, 
+            betas, 
+            alpha_error=alpha_error
+        ),
+        (2, 1, 0)
+    )
+    t_beta = np.transpose(theoretical[..., 1])
+
+    # Getting emperical beta values
+    datasets = np.sort(datasets)
+    def _eppf(p: float, sorted_data: list[float]) -> float:
+        return sorted_data[int(p * len(sorted_data))]
+    eppf = np.vectorize(
+        _eppf,
+        signature="(),(n)->()"
+    )
+
+    def _ecdf(x: float, sorted_data: list[float]) -> float:
+        return np.searchsorted(sorted_data, x) / len(sorted_data)
+    ecdf = np.vectorize(
+        _ecdf,
+        signature="(),(n)->()"
+    )
+
+    assisted = datasets[0]
+    unassisted = datasets[1]
+    cutoff = eppf(1 - alpha_error, unassisted)
+    e_beta = ecdf(cutoff, assisted)
+    beta_diff = e_beta - t_beta
+    return (theoretical, beta_diff)
+
+def save(
+        directory, 
+        theoretical, 
+        beta_difference, 
+        graph_form
+):
+    print("saving...", flush=True)
+    np.savetxt(directory + "theoretical_cutoffs.csv", theoretical[:, :, 0], delimiter=",", fmt="%.3f")
+    np.savetxt(directory + "theoretical_beta.csv", theoretical[:, :, 1], delimiter=",", fmt="%.3f")
+    np.savetxt(directory + "e-t_beta_diff.csv", beta_difference, delimiter=",", fmt="%.3f")
+    plt.savefig(
+        directory + graph_form + ".png",
+        bbox_inches="tight",
+        transparent=False,
+        dpi=1000
+    )
+    plt.show()
+
 def main():
     print("starting...")
-    def get_args(data_root: str):
-        global args
-        global res
-        args = {}
-        res = {}
-        args["res"] = res
-        args["data_paths"] = (
-            f"./data/{data_root}/assisted_5class.npy",
-            f"./data/{data_root}/unassisted_5class.npy",
-        )
-        args["results_directory"] = f"./results/{data_root}/"
-        args["graphing"] = {
-            "colors": ["red", "green"],
-            "dims": (4, 6),     # Counts of rows and cols; Want observers x cases
-            "ratio": (2, 1),    # Horizontal vs. vertical aspect ratio
-            "scale": 1.4
 
-        }
-        args["histogram_bins"] = 30
-        args["alpha_error"] = .05
-
-    get_args("prostate_reader")
+    args = get_args("prostate_reader")
+    res = args["res"]
 
     ## * Fetch datasets
-    def get_data():
-        global datasets
-        print("Fetching data...")
-        datasets = [np.load(data) for data in args["data_paths"]]
-        datasets = np.transpose(np.asarray(datasets), (0, 3, 2, 1)) # (assisted/unassisted, observers, cases, surfaces)
-        print("Got datasets")
-
-    get_data()
+    datasets = get_data(args["data_paths"])
 
     ## * Fit each sample for observers x cases
-    def run_fits():
-        print("Running fits...")
-
-        # res["test"] = test(datasets)
-        # betas = res["test"][0]
-        res["betas"] = np.apply_along_axis(fit, -1, datasets)
-
-        print("Fits finished.")
-    
-    run_fits()
+    res["betas"] = run_fits(datasets)
     betas = res["betas"]
 
 
     ## * Visual confirmation of emperical to theoretical
-    def get_graphs():
-        obs = np.flip(np.linspace(
-            datasets.shape[1] - 1, 0, 
-            num=args["dims"][0],
-            endpoint=False,
-            dtype=int
-        )) # obs should count up
-        cases = np.linspace(
-            datasets.shape[2] - 1, 0,
-            num=args["dims"][1],
-            endpoint=False,
-            dtype=int
-        ) # cases should count down (to match the top-left [0, 0] of axs)
-        fig, axs = plt.subplots(
-            ncols=args["dims"][0], 
-            nrows=args["dims"][1], 
-            figsize=(args["ratio"][0] * args["scale"] * args["dims"][0], args["ratio"][1] * args["scale"] * args["dims"][1]),
-            layout="constrained"
-        )
-        x = np.linspace(0, 1, num=500)
-        constant_indices = True
-        is_cdf = False
-        with np.nditer(
-            axs,
-            flags=[
-                "multi_index",
-                "refs_ok"
-            ],
-            op_flags=[
-                "readwrite"
-            ],
-            op_dtypes=matplotlib.axes._axes.Axes
-        ) as it:
-            for ax in it:
-                ax = ax.item()
-
-                ind = (
-                    obs[it.multi_index[1]], 
-                    cases[it.multi_index[0]]
-                )
-                # Adjust indices to skip None in betas
-                while any([betas[g, *ind] == None for g in range(datasets.shape[0])]):
-                    # increment cases if needed
-                    ind = (ind[0], ind[1] + 1)
-                    constant_indices = False
-
-                ax.set_xlabel(ind[0] + 1)
-                ax.set_ylabel(ind[1] + 1)
-                for group in range(datasets.shape[0]):
-                    # Finally, graph emperical (hist) and theoretical (plot) pdfs for each SARAPE surface
-                    group_ind = (group, *ind)
-                    hist = ax.hist(
-                        datasets[group_ind[0], group_ind[1], group_ind[2]], 
-                        bins=args["histogram_bins"], 
-                        range=(0, 1), 
-                        align="mid",
-                        density=True, # Draw normalized so area == 1
-                        cumulative=is_cdf, # Draw as CDF
-                        color=args["colors"][group],
-                        alpha=.5
-                    )[0]
-
-                    if is_cdf:
-                        y = betas[group_ind[0], group_ind[1], group_ind[2]].cdf(x)
-                    else:
-                        y = betas[group_ind[0], group_ind[1], group_ind[2]].pdf(x)
-
-                    ax.plot(
-                        x, y,
-                        color=args["colors"][group]
-                    )
-
-        # Clear out excess axes display elements
-        with np.nditer(
-            axs,
-            flags=[
-                "multi_index",
-                "refs_ok"
-            ],
-            op_flags=[
-                "readwrite"
-            ],
-            op_dtypes=matplotlib.axes._axes.Axes
-        ) as it:
-            bottom_row = args["dims"][1] - 1
-            left_col = 0
-            for ax in it:
-                # For some god forsaken reason, ax is a zero-dimensional array which python just DOESN'T want to treat right
-                # We use `ndarray.item()` to get the Python scalar value
-                ax = ax.item()
-                is_bottom_row = it.multi_index[0] == bottom_row
-                is_left_col = it.multi_index[1] == left_col
-                ax.tick_params(
-                    axis="both",
-                    which="both",
-                    top=False,
-                    bottom=is_bottom_row,
-                    left=False,
-                    right=False,
-                    labeltop=False,
-                    labelbottom=is_bottom_row,
-                    labelleft=False,
-                    labelright=False,
-                )
-                
-                if constant_indices:
-                    if not is_left_col:
-                        ax.set_ylabel("")
-                    if not is_bottom_row:
-                        ax.set_xlabel("")
-
-        args["graph_form"] = "CDF" if is_cdf else "PDF"
-        fig.suptitle(f"Various SARAPE Emperical vs. Theoretical {args['graph_form']}s")
-        fig.supxlabel("Observer Count")
-        fig.supylabel("Case Count")
-    
-    get_graphs()
+    graph_form = get_graphs(datasets, betas, args["graphing"])
 
 
     ## * Kolmogrov-Smirnov to check theoretical validity within p = .05
-    def run_kstest():
-        def unwrapped_kstest(x: np.ndarray[stats.rv_continuous, float]):
-            '''
-            Apply single-sided kstest from NDArray. Necessary for using apply_along_axis
-            since we can't otherwise use two parallel arrays
-
-            x : NDArray
-                Of the form `[stats.rv_continuous, float64, ..., float64]`
-            
-            Returns
-            -------
-            KstestResult (see `scipy.stats`)
-            '''
-            beta = x[0]
-            if beta == None:
-                return None
-            data = x[1:].astype('f')
-            return stats.kstest(data, beta.cdf)
-
-        combo = np.concatenate((betas[..., np.newaxis], datasets), axis=-1)
-        res["kstest"] = np.apply_along_axis(unwrapped_kstest, -1, combo)
-    
-    run_kstest()
+    res["kstest"] = run_kstest(betas, datasets)
 
 
     ## * Beta comparison
-    def run_emperical_vs_theoretical_comparison():
-        print("Running cutoffs...", flush=True)
-        res["theoretical"] = np.transpose(
-            np.apply_along_axis(
-                lambda betas, alpha_error=.05: compare(*betas, alpha_error=alpha_error),
-                0, 
-                betas, 
-                alpha_error=args["alpha_error"]
-            ),
-            (2, 1, 0)
-        )
-        t_beta = np.transpose(res["theoretical"][..., 1])
-
-        # Getting emperical beta values
-        datasets = np.sort(datasets)
-        def _eppf(p: float, sorted_data: list[float]) -> float:
-            return sorted_data[int(p * len(sorted_data))]
-        eppf = np.vectorize(
-            _eppf,
-            signature="(),(n)->()"
-        )
-
-        def _ecdf(x: float, sorted_data: list[float]) -> float:
-            return np.searchsorted(sorted_data, x) / len(sorted_data)
-        ecdf = np.vectorize(
-            _ecdf,
-            signature="(),(n)->()"
-        )
-
-        assisted = datasets[0]
-        unassisted = datasets[1]
-        cutoff = eppf(1 - args["alpha_error"], unassisted)
-        e_beta = ecdf(cutoff, assisted)
-        res["beta_diff"] = e_beta - t_beta
-    
-    run_emperical_vs_theoretical_comparison
+    res["theoretical"], res["beta_diff"] = run_emperical_vs_theoretical_comparison(betas, res["alpha_error"])
 
 
     ## * Save results
-    def save():
-        print("saving...", flush=True)
-        np.savetxt(args["results_directory"] + "theoretical_cutoffs.csv", res["theoretical"][:, :, 0], delimiter=",", fmt="%.3f")
-        np.savetxt(args["results_directory"] + "theoretical_beta.csv", res["theoretical"][:, :, 1], delimiter=",", fmt="%.3f")
-        np.savetxt(args["results_directory"] + "e-t_beta_diff.csv", np.transpose(res["beta_diff"]), delimiter=",", fmt="%.3f")
-        plt.savefig(
-            args["results_directory"] + args["graph_form"] + ".png",
-            bbox_inches="tight",
-            transparent=False,
-            dpi=1000
-        )
-        plt.show()
-    
-    save()
+    save(args["results_directory"], res["theoretical"], np.transpose(res["beta_diff"]), graph_form)
 
 if __name__ == '__main__':
     main()
