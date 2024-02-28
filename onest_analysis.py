fb926974a9cb056a8e2f1e90ea40d22ea87a6521
blob1234567890 <ben.k.olson@gmail.com>
2024-02-27 22:42:23 -0800

    added thoughts and additional notes

--- onest_analysis.py
+++ onest_analysis.py
@@ -27,7 +27,7 @@ def get_args() -> dict[str, Any]:
             "assisted",
             "unassisted"
         ],
-        "method": "onest",
+        "method": "contest",
         "describe": True,
         "cache": True
     }
